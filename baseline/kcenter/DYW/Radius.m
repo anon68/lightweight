a49f4a353e677919a1a33ee71f235baeed65dd59
anon68 <71723391+anon68@users.noreply.github.com>
2020-12-07 00:13:48 +0800

    Add files via upload

---
+++ baseline/kcenter/DYW/Radius.m
@@ -0,0 +1,6 @@
+function [radius] = Radius(centers, data, z)
+
+    D = pdist2(data,centers);
+    mD = maxk(min(D,[],2),z+1); 
+    radius = mD(z+1,:);
+end
---
+++ baseline/kcenter/MK/Radius.m
@@ -0,0 +1,8 @@
+function [radius] = Radius(centers, data, z)
+%     R1 = size(centers,1);
+%     R2 = size(data,1);
+%     D = sum(data.*data,2)*ones(1,R1) + ones(R2,1)*sum(centers.*centers,2)' - 2*data*centers';
+    D = pdist2(data,centers);
+    mD = maxk(min(D,[],2),z+1); 
+    radius = mD(z+1,:);
+end
